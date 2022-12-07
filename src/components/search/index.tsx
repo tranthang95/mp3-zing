78eb090b3a8d09911438b19880399be1d45234a2
Thang <txthang1995@gmail.com>
2022-12-07 20:19:23 +0700

    feat: init project

---
+++ src/components/search/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Search:React.FC = () => {
+  return (
+    <div className="search">
+      <button>a</button>
+      <button>b</button>
+      <button>v</button>
+      <button>ggg</button>
+    </div>
+  )
+}
+
+export default Search;
---
+++ src/components/side-bar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../img/Zing_logo.png'
+
+
+const SideBar:React.FC = () => {
+  return (
+    <div className="side-bar">
+      <img src={logo} alt="logo zing" />
+      
+    </div>
+  )
+}
+
+export default SideBar;
